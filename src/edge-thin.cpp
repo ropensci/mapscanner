c6a2089859062a882429ff4eaf5c51ec7b61c0b5
mpadge <mark.padgham@email.com>
2019-07-01 21:58:39 +0200

    add src

---
+++ src/edge-thin.cpp
@@ -0,0 +1,11 @@
+#include <Rcpp.h>
+
+
+//' rcpp_edge_thin
+//' @noRd 
+// [[Rcpp::export]]
+Rcpp::NumericVector rcpp_edge_thin (Rcpp::IntegerMatrix image)
+{
+    Rcpp::NumericVector res (10);
+    return res;
+}
