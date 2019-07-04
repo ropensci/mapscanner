227c6800df0dd61672f4e3eb99b383d4268a7dfa
mpadge <mark.padgham@email.com>
2019-07-04 15:24:07 +0200

    :balloon:tests

--- tests/testthat/test-mapscanner.R
+++ tests/testthat/test-mapscanner.R
@@ -1,5 +1,6 @@
 context("basic functionality")
-test_that("we can do something", {
+
+test_that("rectify", {
               f_orig <- system.file ("extdata", "omaha.jpg",
                                      package = "mapscanner")
               expect_true (file.exists (f_orig))
@@ -20,7 +21,19 @@ test_that("we can do something", {
                   magick::image_resize ("25%") %>%
                   magick::image_write (f_modified2)
 
-              system.time (res <- ms_rectify_maps (f_orig2, f_modified2))
-              expect_is (res, "sf")
-              expect_is (res$geometry, "sfc_POLYGON")
+              expect_silent (res_p <- ms_rectify_maps (f_orig2, f_modified2,
+                                                       type = "polygons"))
+              expect_is (res_p, "sf")
+              expect_is (res_p$geometry, "sfc_POLYGON")
+
+              expect_silent (res_h <- ms_rectify_maps (f_orig2, f_modified2,
+                                                       type = "hulls"))
+              expect_is (res_h, "sf")
+              expect_is (res_h$geometry, "sfc_POLYGON")
+              expect_true (all (sf::st_area (res_h) > sf::st_area (res_p)))
+
+              expect_silent (res_1 <- ms_rectify_maps (f_orig2, f_modified2,
+                                                       type = "points"))
+              expect_is (res_1, "sf")
+              expect_is (res_1$geometry, "sfc_MULTIPOINT")
 })
